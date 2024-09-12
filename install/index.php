d1055f4f8c410d9fdb4f7c3d770f768e6a6dcbc9
Nikita Balashov <nikita@example.com>
2024-09-12 09:31:58 +0000

    фикс установки версии модуля

--- install/index.php
+++ install/index.php
@@ -5,11 +5,23 @@ class your_module extends CModule
 {
     public function __construct()
     {
+        
+        //получим путь до модуля
+            $path = str_replace("\\", "/", __FILE__);
+            $path = substr($path, 0, strlen($path) - strlen("/install/index.php"));
+
+        //установка версии модуля
+            include($path."/version.php");
+            if (is_array($arModuleVersion) && array_key_exists("VERSION", $arModuleVersion))
+            {
+                $this->MODULE_VERSION = $arModuleVersion["VERSION"];
+                $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+            }
+
+        //оставльные данные модуля
         $this->MODULE_ID = 'your.module';
-        $this->MODULE_NAME = 'Мой модуль';
+        $this->MODULE_NAME = 'Мой модуль ';
         $this->MODULE_DESCRIPTION = 'Описание модуля';
-        $this->MODULE_VERSION = '1.0.0';
-        $this->MODULE_VERSION_DATE = '2024-09-12';
     }
 
     public function DoInstall()
